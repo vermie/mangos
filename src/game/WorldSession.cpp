4f4fe77593c50c7caf544a8446543b0c55618252
faramir118 <faramir118@gmail.com>
2011-03-03 00:22:50 -0600

    remove unused header

--- src/game/WorldSession.cpp
+++ src/game/WorldSession.cpp
@@ -40,8 +40,6 @@
 #include "Auth/HMACSHA1.h"
 #include "zlib/zlib.h"
 
-#include "AsyncSocket.h"
-
 // select opcodes appropriate for processing in Map::Update context for current session state
 static bool MapSessionFilterHelper(WorldSession* session, OpcodeHandler const& opHandle)
 {
